0c445c54ccfe45f48678a092b9362946c8ff5330
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-05-15 20:55:08 +0300

    Add files via upload

---
+++ GuessingGame150519/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame150519
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" GUSSING GAME");
+            Random pickAword = new Random();
+            int numOfGusses = 0;
+            string[] words = new string[5];
+            words[0] = "same";
+            words[1] = "notsafe";
+            words[2] = "world";
+            words[3] = "map";
+            words[4] = "computer";
+            List<char> charit = new List<char>();
+            string theWordIs = words[pickAword.Next(5)];
+            foreach (char x in theWordIs)
+            {
+                Console.Write(" _ ");
+                
+                charit.Add(x);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"guess number: {numOfGusses}");
+            Console.WriteLine("what is your guss?");
+            char letter = Convert.ToChar( Console.ReadLine());
+            Console.ReadLine();
+        
+
+
+
+
+
+
+
+
+        }
+    }
+}
